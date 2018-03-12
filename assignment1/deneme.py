12fe87db411ecc43399ebc7aa7cb2f369fd72540
Leyza Yortanlı <lyortanli@gmail.com>
2018-03-12 08:55:57 +0300

    gaborfilters

---
+++ assignment1/deneme.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+def build_filters():
+    """ returns a list of kernels in several orientations
+    """
+    filters = []
+    ksize = 31
+    for theta in np.arange(0, np.pi, np.pi / 32):
+        params = {'ksize':(ksize, ksize), 'sigma':1.0, 'theta':theta, 'lambd':15.0,
+                  'gamma':0.02, 'psi':0, 'ktype':cv2.CV_32F}
+        kern = cv2.getGaborKernel(**params)
+        kern /= 1.5*kern.sum()
+        filters.append((kern, params))
+    return filters
+
+def process(img, filters):
+    """ returns the img filtered by the filter list
+    """
+    accum = np.zeros_like(img)
+    for kern,params in filters:
+        fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
+        np.maximum(accum, fimg, accum)
+    return accum
+
+
+#main
+e = cv2.imread('Part2-dataset/QUERY_IMAGES/airplane_251_0175.jpg')
+filters = build_filters()
+p = process(e, filters)
+cv2.imshow('img',p)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+for filter in filters:
+    cv2.imshow('img', filter)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
